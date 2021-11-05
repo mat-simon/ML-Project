5a4538228a2b39637f63d10057df5e041a5afc00
Mathew Simon <mat-simon@users.noreply.github.com>
2021-11-05 03:06:30 -0700

    Filtered data down to data for digits 4 and 8 only Might want the normalization, not sure yet

--- random_forrest.py
+++ random_forrest.py
@@ -1,24 +1,23 @@
 import numpy as np
 import scipy.io
 
-dictionary_data = scipy.io.loadmat('MNISTmini.mat')
 
-print("dictionary keys:", dictionary_data.keys())
-train_fea1 = np.array(dictionary_data['train_fea1'])
-print("size of train_fea1 np array: ", train_fea1.size)
-print(train_fea1)
+def main():
+    dictionary_data = scipy.io.loadmat('MNISTmini.mat')
+    print("dictionary keys:", dictionary_data.keys())
 
-train_gnd1 = np.array(dictionary_data['train_gnd1'])
-print("=========================================")
-print("size of train_gnd1:", train_gnd1.size)
-print(train_gnd1)
+    train_fea1 = np.array(dictionary_data['train_fea1'])
+    print("shape of train_fea1 np array:", train_fea1.shape)
 
-test_fea1 = np.array(dictionary_data['test_fea1'])
-print("=========================================")
-print("size of test_fea1", test_fea1.size)
-print(test_fea1)
+    train_gnd1 = np.array(dictionary_data['train_gnd1'])
+    print("shape of train_gnd1:", train_gnd1.shape)
 
-test_gnd1 = np.array(dictionary_data['test_gnd1'])
-print("=========================================")
-print("size of test_gnd1", test_gnd1.size)
-print(test_gnd1)
+    test_fea1 = np.array(dictionary_data['test_fea1'])
+    print("shape of test_fea1:", test_fea1.shape)
+
+    test_gnd1 = np.array(dictionary_data['test_gnd1'])
+    print("shape of test_gnd1:", test_gnd1.shape)
+
+
+if __name__ == '__main__':
+    main()
