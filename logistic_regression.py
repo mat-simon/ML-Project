4b7fc664b00bd67138a7d06a4780ece49ed55965
Alberto <albertovalleflores99@gmail.com>
2021-11-04 20:46:36 -0700

    move code to main function

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,24 +1,30 @@
 import numpy as np
 import scipy.io
 
-dictionary_data = scipy.io.loadmat('MNISTmini.mat')
 
-print("dictionary keys:", dictionary_data.keys())
-train_fea1 = np.array(dictionary_data['train_fea1'])
-print("size of train_fea1 np array: ", train_fea1.size)
-print(train_fea1)
+def main():
+    dictionary_data = scipy.io.loadmat('MNISTmini.mat')
 
-train_gnd1 = np.array(dictionary_data['train_gnd1'])
-print("=========================================")
-print("size of train_gnd1:", train_gnd1.size)
-print(train_gnd1)
+    print("dictionary keys:", dictionary_data.keys())
+    train_fea1 = np.array(dictionary_data['train_fea1'])
+    print("size of train_fea1 np array: ", train_fea1.size)
+    print(train_fea1)
 
-test_fea1 = np.array(dictionary_data['test_fea1'])
-print("=========================================")
-print("size of test_fea1", test_fea1.size)
-print(test_fea1)
+    train_gnd1 = np.array(dictionary_data['train_gnd1'])
+    print("=========================================")
+    print("size of train_gnd1:", train_gnd1.size)
+    print(train_gnd1)
 
-test_gnd1 = np.array(dictionary_data['test_gnd1'])
-print("=========================================")
-print("size of test_gnd1", test_gnd1.size)
-print(test_gnd1)
+    test_fea1 = np.array(dictionary_data['test_fea1'])
+    print("=========================================")
+    print("size of test_fea1", test_fea1.size)
+    print(test_fea1)
+
+    test_gnd1 = np.array(dictionary_data['test_gnd1'])
+    print("=========================================")
+    print("size of test_gnd1", test_gnd1.size)
+    print(test_gnd1)
+
+
+if __name__ == '__main__':
+    main()
