2691b3a40e788b0c74add291ef9df97a3d88500e
Mathew Simon <mat-simon@users.noreply.github.com>
2021-11-08 18:44:28 -0800

    Now using CV on train data

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,8 +1,9 @@
 import numpy as np
 import scipy.io
 import util
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
 # from sklearn.preprocessing import StandardScaler
 
 
@@ -28,13 +29,21 @@ def main():
     ###
 
     train_gnd1_input = np.ravel(train_gnd1)
-    model = LogisticRegression(C=.01, penalty="l2", solver="liblinear", tol=0.1)
-    model.fit(train_fea1, train_gnd1_input)
 
-    train_score = model.score(train_fea1, train_gnd1)
-    val_score = model.score(test_fea1, test_gnd1)
-    print("score on train data:", train_score)
-    print("score on validation data:", val_score)
+    # model = LogisticRegression(C=48.8, penalty="l2", solver="sag", tol=0.001, max_iter=1000)
+    # model.fit(train_fea1, train_gnd1_input)
+    # train_score = model.score(train_fea1, train_gnd1)
+    # val_score = model.score(test_fea1, test_gnd1)
+    # print("LR score on train data:", train_score)
+    # print("LR score on validation data:", val_score)
+
+
+    Cs = [x / 10 for x in range(1, 305)]
+    model = LogisticRegressionCV(Cs=Cs, n_jobs=-1, penalty="l2", solver="liblinear", tol=.001)
+    model.fit(train_fea1, train_gnd1_input)
+    # print('Max:', model.scores_[1].mean(axis=0).max())
+    print("CV train score:", model.score(train_fea1, train_gnd1))
+    print("CV validation score:", model.score(test_fea1, test_gnd1))
 
     # ax = plt.gca()
     # rfc_disp = RocCurveDisplay.from_estimator(rfc, X_test, y_test, ax=ax, alpha=0.8)
