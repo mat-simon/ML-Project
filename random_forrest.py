cbfd02e78c8492289dabf1515d2735923b8a0842
Alberto <albertovalleflores99@gmail.com>
2021-11-09 18:21:26 -0800

    update random forest

--- random_forrest.py
+++ random_forrest.py
@@ -1,18 +1,19 @@
 import numpy as np
 import scipy.io
-import math
 import util
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 
 
-def render_acc_trees_graph(train, truth, test, test_truth):
+def graph_acc_trees(train, truth, test, test_truth):
     clfs = []
-    num_trees = list(range(1, 200))
+    num_trees = list(range(1, 201))
 
-    for i in range(1, 200):
+    for i in range(1, 11):
         clf = RandomForestClassifier(n_estimators=i, n_jobs=-1)
         clf.fit(train, truth)
         clfs.append(clf)
@@ -22,12 +23,12 @@ def render_acc_trees_graph(train, truth, test, test_truth):
     ax.set_xlabel("Number of trees")
     ax.set_ylabel("Error")
     ax.set_title("Error vs number of trees for training and testing sets")
-    ax.plot(num_trees, train_scores, marker="o", label="train",
+    ax.plot(num_trees, train_scores, label="train",
             drawstyle="steps-post")
-    ax.plot(num_trees, test_scores, marker="o", label="test",
+    ax.plot(num_trees, test_scores, label="test",
             drawstyle="steps-post")
     ax.legend()
-    fig.savefig("error_trees.png")
+    fig.savefig("error_trees2.png")
 
 
 def prune_tree(train, truth):
@@ -57,67 +58,81 @@ def prune_tree(train, truth):
 
 
 def feature_importance(train, truth):
-    sel = SelectFromModel(RandomForestClassifier(n_estimators=1))
+    print("Feature importance for every pixel:")
+    sel = SelectFromModel(RandomForestClassifier(n_estimators=100))
     sel.fit(train, truth)
     np_fi = np.array(sel.get_support())
     print(np_fi.reshape(10, 10))
-    # selected_feat = train.columns[(sel.get_support())]
-    # len(selected_feat)
-    # print(selected_feat)
-    # pd.series(sel.estimator_, feature_importances_,.ravel()).hist()
-
     return sel
 
-def main():
-    dictionary_data = scipy.io.loadmat('MNISTmini.mat')
-    # print("dictionary keys:", dictionary_data.keys())
 
-    train_fea1 = np.array(dictionary_data['train_fea1'])
-    # print("shape of train_fea1 np array:", train_fea1.shape)
-
-    train_gnd1 = np.array(dictionary_data['train_gnd1'])
-    # print("shape of train_gnd1:", train_gnd1.shape)
-
-    test_fea1 = np.array(dictionary_data['test_fea1'])
-    # print("shape of test_fea1:", test_fea1.shape)
-
-    test_gnd1 = np.array(dictionary_data['test_gnd1'])
-    # print("shape of test_gnd1:", test_gnd1.shape)
-
-    # delete rows that are not 4 or 8 (our assigned digits to classify)
-    train_fea1, train_gnd1 = util.reduce_data(train_fea1, train_gnd1, 4, 8)
-    test_fea1, test_gnd1 = util.reduce_data(test_fea1, test_gnd1, 4, 8)
-    # num_features = math.log(train_gnd1.shape[0], 2)
-    # feature_importance(train_fea1, train_gnd1.ravel())
-
-    # alpha = prune_tree( train_fea1, train_gnd1.ravel())
-
-    render_acc_trees_graph(train_fea1, train_gnd1.ravel(), test_fea1, test_gnd1.ravel())
+def train_model(train, truth, test, test_truth):
+    """
+    Trains a single model using the RandomForestClassifier and prints score
+    result
+    Modify hyperparameters for best accuracy
+    :param train: numpy array with the MNIST image data for training
+    :param truth: array of 8s and 4s true values for training data
+    :param test: numpy array with the MNIST image data for testing
+    :param test_truth: array of 8s and 4s true values for test data
+    :return: trained model
+    """
     clf = RandomForestClassifier(n_estimators=100, n_jobs=-1)
-    clf.fit(train_fea1, train_gnd1.ravel())
+    clf.fit(train, truth)
+    result = clf.score(test, test_truth)
+    print("Score: ", result)
+
+    return clf
 
 
-    result = clf.score(test_fea1, test_gnd1.ravel())
-    print(result)
+def get_data():
+    print("Loading data...")
+    try:
+        dictionary_data = scipy.io.loadmat('MNISTmini.mat')
+        train_fea1 = np.array(dictionary_data['train_fea1'])
+        train_gnd1 = np.array(dictionary_data['train_gnd1'])
+        test_fea1 = np.array(dictionary_data['test_fea1'])
+        test_gnd1 = np.array(dictionary_data['test_gnd1'])
+        print("success")
+    except Exception as e:
+        print(e)
 
-    # names = list(range(1, 100))
-    #
-    # print("Features sorted by score:")
-    # print(sorted(zip(map(lambda x: round(x, 4), clf.feature_importances_), names), reverse=True))
-    #
-    # print("Best accuracy by training:")
-    # best_accuracy = 0
-    # best_accuracy_inx = 0
-    # for i in range(1, 100):
-    #     clf = RandomForestClassifier(n_estimators=1, max_features=i)
-    #     clf.fit(train_fea1, train_gnd1.ravel())
-    #     result = clf.score(test_fea1, test_gnd1.ravel())
-    #     if best_accuracy < result:
-    #         best_accuracy = result
-    #         best_accuracy_inx = i
-    #     # print(i, result)
-    #
-    # print("Best accuracy:", best_accuracy_inx, best_accuracy)
+    print("Reducing data to 8s and 4s...")
+    try:
+        train_fea1, train_gnd1 = util.reduce_data(train_fea1, train_gnd1, 4, 8)
+        test_fea1, test_gnd1 = util.reduce_data(test_fea1, test_gnd1, 4, 8)
+        print("success")
+    except Exception as e:
+        print(e)
+
+    return train_fea1, train_gnd1.ravel(), test_fea1, test_gnd1.ravel()
+
+
+def feature_heatmap(model):
+    feature_scores = np.array(model.feature_importances_).reshape(10, 10)
+    fig, ax = plt.subplots()
+    im = ax.imshow(feature_scores)
+    ax.set_xticks(np.arange(10))
+    ax.set_yticks(np.arange(10))
+    fig.savefig('heatmap2.png')
+
+def main():
+
+    train, truth, test, test_truth = get_data()
+    # feature_importance(train, truth)
+    clf = train_model(train, truth, test, test_truth)
+    # feature_heatmap(clf)
+
+    params = {'n_estimators': [1, 2, 3, 5, 10, 20, 50, 100, 200],
+              'max_features': [x for x in range(2, 25)],
+              # 'criterion': ['gini', 'entropy']
+              }
+
+    model = GridSearchCV(RandomForestClassifier(n_jobs=-1), params)
+    model.fit(train, truth)
+    print(f"Best estimator: {model.best_estimator_})")
+    print(f"Accuracy = {model.best_score_}")
+    print("Test score:", model.score(test, test_truth))
 
 
 if __name__ == '__main__':
