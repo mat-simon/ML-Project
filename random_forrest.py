8ace17f2eab9cefa9ce4986f8bf5e9bbfed312f2
Mathew Simon <mat-simon@users.noreply.github.com>
2021-11-10 14:35:49 -0800

    more params for tree grid search added sorted dict of accuracy to params

--- random_forrest.py
+++ random_forrest.py
@@ -8,16 +8,16 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 # from sklearn.model_selection import GridSearchCV
 
 
-def graph_acc_trees(train, truth, test, test_truth):
+def graph_acc_trees(train_X, train_Y, validation_X, validation_Y):
     clfs = []
-    num_trees = list(range(1, 201))
+    num_trees = list(range(1, 501))
 
-    for i in range(1, 11):
+    for i in num_trees:
         clf = RandomForestClassifier(n_estimators=i, n_jobs=-1)
-        clf.fit(train, truth)
+        clf.fit(train_X, train_Y)
         clfs.append(clf)
-    train_scores = [1 - clf.score(train, truth) for clf in clfs]
-    test_scores = [1 - clf.score(test, test_truth) for clf in clfs]
+    train_scores = [1 - clf.score(train_X, train_Y) for clf in clfs]
+    test_scores = [1 - clf.score(validation_X, validation_Y) for clf in clfs]
     fig, ax = plt.subplots()
     ax.set_xlabel("Number of trees")
     ax.set_ylabel("Error")
@@ -36,7 +36,7 @@ def prune_tree(train, truth):
     path = clf.cost_complexity_pruning_path(train, truth)
     ccp_alphas, impurities = path.ccp_alphas, path.impurities
     fig, ax = plt.subplots()
-    ax.plot(ccp_alphas[:-1], impurities[:-1], marker="o",
+    ax.plot(ccp_alphas[:-1], impurities[:-1], #marker="o",
             drawstyle="steps-post")
     ax.set_xlabel("effective alpha")
     ax.set_ylabel("total impurity of leaves")
@@ -75,20 +75,28 @@ def train_model(train_X, train_Y, validation_X, validation_Y):
     :param validation_Y: array of ground truth values for validation_X
     :return: trained model
     """
-    n_estimators_arr = [1, 2, 3, 5, 10, 100, 200]
-    max_features = [x for x in range(1, 90)]
+    n_estimators_arr = [x for x in range(10, 300, 10)]
+    max_features = [10, 20, 30, 40, 50, 100]
+    max_depth = [x for x in range(1, 10)]
     best_model = ''
     best_acc = 0
-    for max_fea in max_features:
-        for n in n_estimators_arr:
-            clf = RandomForestClassifier(n_estimators=n, max_features=max_fea, n_jobs=-1)
-            clf.fit(train_X, train_Y)
-            accuracy = clf.score(validation_X, validation_Y)
-            if accuracy > best_acc:
-                best_model = "n_estimators = " + str(n) + ", max_features = " + str(max_fea)
-                best_acc = accuracy
+    acc_dict = {}
+    for n in n_estimators_arr:
+        for max_fea in max_features:
+            for depth in max_depth:
+                print("n:", n, "max_fea:", max_fea, "depth:", depth)
+                clf = RandomForestClassifier(n_estimators=n, max_features=max_fea, n_jobs=-1, max_depth=depth)
+                clf.fit(train_X, train_Y)
+                accuracy = clf.score(validation_X, validation_Y)
+                acc_dict[accuracy] = ['n:', n, 'max_fea:', max_fea, 'depth:', depth]
+                if accuracy > best_acc:
+                    best_model = "n_estimators = " + str(n) + ", max_features = " + str(max_fea) + ", max depth = " + str(depth)
+                    best_acc = accuracy
     print("parameters for best model: ", best_model)
     print("best accuracy on validation set: ", best_acc)
+    temp = sorted(acc_dict, reverse=True)
+    acc_dict = {key: acc_dict[key] for key in temp}
+    print("acc_dict", acc_dict)
     return clf
 
 
@@ -131,6 +139,7 @@ def feature_heatmap(model):
 def main():
     train_X, train_Y, validation_X, validation_Y, test_fea1, test_gnd1 = get_data()
     clf = train_model(train_X, train_Y, validation_X, validation_Y)
+    # graph_acc_trees(train_X, train_Y, validation_X, validation_Y)
     # feature_importance(train_X, train_Y)
     # feature_heatmap(clf)
 
