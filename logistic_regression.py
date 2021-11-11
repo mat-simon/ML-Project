5136abce8679d160aad043e5a7e339f98ff6fdc8
Mathew Simon <mat-simon@users.noreply.github.com>
2021-11-11 02:21:51 -0800

    added and fixed plots, added ROC, fixed best_model

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,8 +1,13 @@
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+import util
 import numpy as np
 import scipy.io
-import util
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
-# import matplotlib.pyplot as plt
+from sklearn.metrics import RocCurveDisplay, plot_roc_curve
+from sklearn import metrics
 # from sklearn.linear_model import LogisticRegressionCV
 # from sklearn.model_selection import GridSearchCV
 # from sklearn.preprocessing import StandardScaler
@@ -27,39 +32,92 @@ def main():
     # take second 1000 rows of 4's and second 1000 rows of 8's to create validation set
     validation_X = train_fea1[np.r_[1000:2000, 6842:7842], :]
     validation_Y = train_gnd1[np.r_[1000:2000, 6842:7842], :]
-
     print("shape of train set:", train_X.shape)
     print("shape of validation set:", validation_X.shape, "\n")
 
-    #     # data values from [0, 255] to [0, 1]
-    #     #train_fea1 = np.divide(train_fea1, 255)
-    #     #test_fea1 = np.divide(test_fea1, 255)
+    # trying out standard scalar
+    # scalar = StandardScaler()
+    # scalar.fit(train_X)
+    # scalar.transform(train_X)
+    # scalar.fit(validation_X)
+    # scalar.transform(validation_X)
 
-    penalty_arr = ["l2"]
-    best_model = ''
+    # Fix C, graph tolerance vs accuracy
+    t_train_acc = []
+    t_validation_acc = []
+    tol_arr = [1, 0.5, 0.1, 0.01, 0.001, 0.0001, 0.00001]
+    for tol in tol_arr:
+        model = LogisticRegression(C=0.01, penalty='l2', solver='liblinear', tol=tol)
+        model.fit(train_X, train_Y.ravel())
+        train_acc = model.score(train_X, train_Y)
+        val_acc = model.score(validation_X, validation_Y)
+        t_train_acc.append(train_acc*100)
+        t_validation_acc.append(val_acc*100)
+    fig, ax = plt.subplots()
+    plt.xscale("log")
+    ax.set_xlabel("Tolerance")
+    ax.set_ylabel("Accuracy %")
+    ax.set_title("Accuracy vs Tolerance, fixed 1/λ = .01")
+    ax.plot(tol_arr, t_train_acc, label="train", marker='o')
+    ax.plot(tol_arr, t_validation_acc, label="validation", marker='o')
+    ax.legend()
+    fig.savefig("Err_vs_tol_Log_Reg.png")
+    plt.show()
+
+    # Fix tolerance, graph C vs accuracy
+    c_train_acc = []
+    c_validation_acc = []
+    c_arr = [.00000001, .0000001, .00001, .0001, .001, .01, .1, 1, 10, 100]
+    for c in c_arr:
+        model = LogisticRegression(C=c, penalty='l2', solver='liblinear', tol=tol)
+        model.fit(train_X, train_Y.ravel())
+        train_acc = model.score(train_X, train_Y)
+        val_acc = model.score(validation_X, validation_Y)
+        c_train_acc.append(train_acc*100)
+        c_validation_acc.append(val_acc*100)
+    fig, ax = plt.subplots()
+    plt.xscale("log")
+    ax.set_xlabel("1/λ")
+    ax.set_ylabel("Accuracy %")
+    ax.set_title("Accuracy vs 1/λ (C)")
+    ax.plot(c_arr, c_train_acc, label="train", marker='o')
+    ax.plot(c_arr, c_validation_acc, label="validation", marker='o')
+    ax.legend()
+    fig.savefig("Err_vs_C_Log_Reg.png")
+    plt.show()
+
+    # small grid search (zooming in based on graphs above) over C and tol for best model overall
+    tol_arr = [.000001] #[x/100 for x in range(1, 50, 1)]
+    c_arr = [10**-20] #[x/10000 for x in range(1, 100, 1)]
+    best_params = ''
     best_acc = 0
-    for penalty in penalty_arr:
-        for i in range(1, 30000):
-            c = i / 1000
-            model = LogisticRegression(C=c, penalty=penalty, solver='liblinear', tol=0.0001)
+    best_model = None
+    print("training LogisticRegression...")
+    for c in c_arr:
+        for tol in tol_arr:
+            model = LogisticRegression(C=c, penalty='l2', solver='liblinear', tol=tol)
             model.fit(train_X, train_Y.ravel())
-            accuracy = model.score(validation_X, validation_Y)
-            if accuracy > best_acc:
-                best_model = "C = " + str(c) + ", penalty = " + penalty + ", solver = 'liblinear', tol = 0.0001"
-                best_acc = accuracy
-    print("parameters for best model: ", best_model)
-    print("best accuracy on validation set: ", best_acc)
+            val_acc = model.score(validation_X, validation_Y)
+            if val_acc > best_acc:
+                best_model = model
+                best_params = "C = " + str(c) + ", penalty = L2, solver = 'liblinear', tol = " + str(tol)
+                best_acc = val_acc
+    # report best model and final accuracy on test set
+    print("parameters for best model: ", best_params)
+    print("best model validation accuracy: ", best_acc)
+    print(f"accuracy on test set: {best_model.score(test_fea1, test_gnd1)}")
 
-    ### preprocessing steps
-    # data values from [0, 255] to [0, 1]
-    # train_fea1 = np.divide(train_fea1, 255)
-    ###
+    # ROC curve on test data
+    RocCurveDisplay.from_estimator(best_model.fit(train_X, train_Y.ravel()), test_fea1, test_gnd1.ravel())
+    fig, ax = plt.subplots()
+    fig.savefig("ROC.png")
+    plt.show()
 
 
 if __name__ == '__main__':
     main()
 
-### Ignore below, just for learning GridSearchCV
+### Ignore below, code for learning GridSearchCV()
 
 # model = LogisticRegressionCV(Cs=10, n_jobs=-1, penalty='elasticnet', solver='saga', tol=0.001, l1_ratios=[0.0, 0.25, 0.5, 0.75, 1])
 # model.fit(train_fea1, train_gnd1_input)
--- random_forrest.py
+++ random_forrest.py
@@ -9,25 +9,24 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
 
 def graph_acc_trees(train_X, train_Y, validation_X, validation_Y):
+    # Fix depth, graph number of trees vs accuracy
     clfs = []
-    num_trees = list(range(1, 201))
-
+    num_trees = list(range(1, 501))
     for i in num_trees:
         clf = RandomForestClassifier(n_estimators=i, n_jobs=-1)
         clf.fit(train_X, train_Y)
         clfs.append(clf)
-    train_scores = [1 - clf.score(train_X, train_Y) for clf in clfs]
-    validation_scores = [1 - clf.score(validation_X, validation_Y) for clf in clfs]
+    train_scores = [clf.score(train_X, train_Y)*100 for clf in clfs]
+    validation_scores = [clf.score(validation_X, validation_Y)*100 for clf in clfs]
     fig, ax = plt.subplots()
     ax.set_xlabel("Number of trees")
-    ax.set_ylabel("Error")
-    ax.set_title("Error vs number of trees for training and validation sets")
-    ax.plot(num_trees, train_scores, label="train",
-            drawstyle="steps-post")
-    ax.plot(num_trees, validation_scores, label="validation",
-            drawstyle="steps-post")
+    ax.set_ylabel("Accuracy %")
+    ax.set_title("Number of trees vs Accuracy")
+    ax.plot(num_trees, train_scores, label="train", marker='o', markersize=2)
+    ax.plot(num_trees, validation_scores, label="validation", marker='o', markersize=2)
     ax.legend()
-    fig.savefig("error_trees2.png")
+    fig.savefig("accuracy_trees.png")
+    plt.show()
 
 
 def prune_tree(train, truth):
@@ -56,13 +55,11 @@ def prune_tree(train, truth):
     return ccp_alphas[-1]
 
 
-def feature_importance(train_X, train_Y):
+def feature_importance(model):
     print("Feature importance for every pixel:")
-    sel = SelectFromModel(RandomForestClassifier(n_estimators=100))
-    sel.fit(train_X, train_Y)
-    np_fi = np.array(sel.get_support())
-    print(np_fi.reshape(10, 10))
-    return sel
+    # fi = np.array(SelectFromModel(model).get_support())
+    # print(fi.reshape(10, 10))
+    return model
 
 
 def train_model(train_X, train_Y, validation_X, validation_Y):
@@ -75,29 +72,31 @@ def train_model(train_X, train_Y, validation_X, validation_Y):
     :param validation_Y: array of ground truth values for validation
     :return: trained model
     """
-    n_estimators_arr = [x for x in range(5, 50, 10)]
-    max_features = [10]     # ['sqrt', 'log2']
-    max_depth = [x for x in range(1, 50)]
-    best_model = ''
+    n_estimators_arr = [5] #[x for x in range(1, 300, 1)]
+    max_features = ['sqrt', 'log2']
+    max_depth = [10] #[x for x in range(1, 50)]
+    best_params = ''
     best_acc = 0
+    best_model = None
     acc_dict = {}
     for n in n_estimators_arr:
         for max_fea in max_features:
             for depth in max_depth:
                 # print("n:", n, "max_fea:", max_fea, "depth:", depth)
-                clf = RandomForestClassifier(n_estimators=n, max_features=max_fea, n_jobs=-1, max_depth=depth)
-                clf.fit(train_X, train_Y)
-                accuracy = clf.score(validation_X, validation_Y)
+                model = RandomForestClassifier(n_estimators=n, max_features=max_fea, n_jobs=-1, max_depth=depth)
+                model.fit(train_X, train_Y)
+                accuracy = model.score(validation_X, validation_Y)
                 acc_dict[accuracy] = [f'n = {n}', f'max_fea = {max_fea}', f'depth = {depth}']
                 if accuracy > best_acc:
-                    best_model = "n_estimators = " + str(n) + ", max_features = " + str(max_fea) + ", max depth = " + str(depth)
+                    best_model = model
+                    best_params = "n_estimators = " + str(n) + ", max_features = " + str(max_fea) + ", max depth = " + str(depth)
                     best_acc = accuracy
-    print("parameters for best model: ", best_model)
+    print("parameters for best model: ", best_params)
     print("best accuracy on validation set: ", best_acc)
     temp = sorted(acc_dict, reverse=True)
     acc_dict = {key: acc_dict[key] for key in temp}
     print("acc_dict", acc_dict)
-    return clf
+    return model
 
 
 def get_data():
@@ -129,19 +128,21 @@ def get_data():
 
 
 def feature_heatmap(model):
-    feature_scores = np.array(model.feature_importances_).reshape(10, 10)
+    feature_scores = np.array(model.feature_importances_).reshape(10, 10).T
     fig, ax = plt.subplots()
-    im = ax.imshow(feature_scores)
+    heatmap = ax.imshow(feature_scores)
     ax.set_xticks(np.arange(10))
     ax.set_yticks(np.arange(10))
-    fig.savefig('heatmap2.png')
+    ax.set_title('Pixel importance')
+    plt.colorbar(heatmap)
+    fig.savefig('heatmap.png')
 
 def main():
     train_X, train_Y, validation_X, validation_Y, test_fea1, test_gnd1 = get_data()
-    clf = train_model(train_X, train_Y, validation_X, validation_Y)
-    # graph_acc_trees(train_X, train_Y, validation_X, validation_Y)
-    # feature_importance(train_X, train_Y)
-    # feature_heatmap(clf)
+    model = train_model(train_X, train_Y, validation_X, validation_Y)
+    graph_acc_trees(train_X, train_Y, validation_X, validation_Y)
+    model = feature_importance(model)
+    feature_heatmap(model)
 
 
 if __name__ == '__main__':
