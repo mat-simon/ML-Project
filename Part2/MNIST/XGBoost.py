8a41f281e423dd37b9999f25803f938d8b01578c
Mathew Simon <mat-simon@users.noreply.github.com>
2021-12-02 00:05:17 -0800

    fixed label "10", need to edit params

--- Part2/MNIST/XGBoost.py
+++ Part2/MNIST/XGBoost.py
@@ -6,9 +6,16 @@ import matplotlib.pyplot as plt
 
 def main():
     train, test = get_data('MNIST.mat')
+    for value in test.get_label():
+        if value not in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+            print("wtf?", value)
+
     param = {'num_class': 10, 'max_depth': 2, 'eta': 1, 'objective': 'multi:softmax'}
-    num_round = 2
-    bst = xgb.train(param, train, num_round)
+    num_round = 12
+    early_stopping = 50
+    eval_list = [(train, "train"), (test, "test")]
+    bst = xgb.train(param, train, num_round, evals=eval_list, early_stopping_rounds=early_stopping, verbose_eval=True)
+
     # make prediction
     preds = bst.predict(test)
     print(preds)
@@ -24,6 +31,14 @@ def get_data(file):
     train_Y = np.array(dictionary_data['train_gnd'])
     test_X = np.array(dictionary_data['test_fea'])
     test_Y = np.array(dictionary_data['test_gnd'])
+
+    # replace label "10" from Matlab with label of 0
+    for i in range(len(train_Y)):
+        if train_Y[i] == 10:
+            train_Y[i] = 0
+    for i in range(len(test_Y)):
+        if test_Y[i] == 10:
+            test_Y[i] = 0
 
     dtrain = xgb.DMatrix(train_X, train_Y)
     dtest = xgb.DMatrix(test_X, test_Y)
