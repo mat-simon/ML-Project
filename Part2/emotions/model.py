9965dcda13e89f97b7cf0212dd96221e0e9ef887
Mathew Simon <mat-simon@users.noreply.github.com>
2021-12-09 05:10:10 -0800

    adding gridsearch and cv functions

--- Part2/emotions/model.py
+++ Part2/emotions/model.py
@@ -1,6 +1,6 @@
 from catboost import CatBoostClassifier, Pool, cv
 from sklearn import metrics
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
 import numpy as np
 from sklearn import preprocessing
 import seaborn as sns
@@ -41,20 +41,18 @@ def main():
     print("Loading data..")
     train_x = np.array(np.load('data/train_x.npy'))
     train_y = np.array(np.load('data/train_y.npy'))
-    print(train_x.shape, train_y.shape)
+    # train_x = np.array(np.load('data/training_x.npy'))
+    # train_y = np.array(np.load('data/training_y.npy'))
+    print("train_x:", train_x.shape)
+    print("train_y:", train_y.shape)
     test_x = np.array(np.load('data/test_x.npy'))
     test_y = np.array(np.load('data/test_y.npy'))
 
-    # train_80_x, val_20_x, train_80_y, val_20_y = train_test_split(train_full,
-    #                                                   truth_full,
-    #                                                   test_size=0.2,
-    #                                                   random_state=42)
-    # print("Shape of train and test data:")
-    # print(train_x.shape, train_y.shape)
-    # print(val_x.shape, val_y.shape)
-
+    train_80_x, val_20_x, train_80_y, val_20_y = train_test_split(train_x,
+                                                                  train_y,
+                                                                  test_size=0.2,
+                                                                  random_state=42)
     # Pre-processing data
-    print("Encoding labels to numerical values...")
     le = preprocessing.LabelEncoder()
     le.fit(train_y)
     train_labels_encoded = le.transform(train_y)
@@ -63,35 +61,79 @@ def main():
 
     train_y, test_y = train_labels_encoded, test_labels_encoded
 
-    print("Creating Pool from training dataset...")
-    train_pool = Pool(data=train_x, label=train_y,
-                      cat_features=None, weight=None,
-                      thread_count=-1)
-
-    # simple_model = CatBoostClassifier(iterations=1000,
-    #                                   loss_function='MultiClass',
-    #                                   task_type='GPU',
-    #                                   devices='0:1')
-
-    params = {
-        'loss_function': 'MultiClass',
-        'iterations': 500,
-        'task_type': 'GPU',
-
-    }
-    # print("Training model...")
-    model = cv(
-        params=params,
-        pool=train_pool,
-        fold_count=5,
-        shuffle=True,
-        partition_random_seed=0,
-        verbose=10
+    train_pool = Pool(
+        data=train_x, label=train_y,
+        cat_features=None, weight=None,
+        thread_count=-1
     )
+    train_80_pool = Pool(
+        data=train_80_x, label=train_80_y,
+        cat_features=None, weight=None,
+        thread_count=-1
+    )
+    val_20_pool = Pool(
+        data=val_20_x, label=val_20_y,
+        cat_features=None, weight=None,
+        thread_count=-1
+    )
+
+    simple_model = CatBoostClassifier(
+        iterations=10,
+        loss_function='MultiClass',
+        od_type='IncToDec',
+        od_pval=.001,
+        task_type='GPU'
+    )
+    simple_model.fit(train_80_pool, eval_set=val_20_pool, verbose=100)
+    # print(simple_model.score(test_x, test_y))
+
+    # Catboost gridsearch
+    # param_grid = {'iterations': [1000],
+    #               'task_type': ['GPU'],
+    #               'depth': [6, 8, 11, 13],
+    #               'verbose': [10]}
+    # simple_model.grid_search(
+    #     param_grid,
+    #     train_x,
+    #     train_y,
+    #     cv=5,
+    #     shuffle=True,
+    #     verbose=10
+    # )
+
+    # Catboost cv
+    # params = {
+    #     'loss_function': 'MultiClass',
+    #     'iterations': 8000,
+    #     'task_type': 'GPU'
+    #
+    # }
+    # # print("Training model...")
+    # model = cv(
+    #     params=params,
+    #     pool=train_pool,
+    #     fold_count=5,
+    #     shuffle=True,
+    #     partition_random_seed=0,
+    #     verbose=100
+    # )
+
+    # # SKlearn GridSearchCV
+    # params = {
+    #     'iterations': [100],
+    #     'depth': [1, 2, 3, 4, 5, 6, 7, 8]
+    # }
+    # model = GridSearchCV(CatBoostClassifier(
+    #     task_type='GPU',
+    #     verbose=50),
+    #     params)
+    # model.fit(train_x, train_y)
+    # print(f"Best estimator: {model.best_params_})")
+    # print(f"Score: {model.best_score_}")
+    # print("Test score:", model.score(test_x, test_y))
 
     # uncomment to save model
     # model.save_model("model_v3.cbm")
-
     # generate_metrics(model, test_x, test_y)
 
 
--- Part2/superconductor/model.py
+++ Part2/superconductor/model.py
@@ -1,6 +1,7 @@
-from catboost import CatBoostRegressor, Pool, cv, CatBoost
+from catboost import CatBoostRegressor, Pool
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 from sklearn import preprocessing
 import numpy as np
 from numpy import genfromtxt
@@ -19,7 +20,36 @@ def main():
     print("train:", train_x.shape)
     print("test:", test_x.shape)
 
+    train_pool = Pool(
+        data=train_x, label=train_y,
+        cat_features=None, weight=None,
+        thread_count=-1
+    )
 
+    # model = CatBoostRegressor(
+    #     iterations=1000,
+    #     learning_rate=1,
+    #     depth=10,
+    #     od_type='IncToDec',
+    #     od_pval=.01,
+    #     task_type='GPU',
+    #     verbose=100
+    # )
+    # model.fit(train_pool)
+    # print("test score:", model.score(test_x, test_y))
+
+    params = {
+        'iterations': [1000],
+        'depth': [14]
+    }
+    model = GridSearchCV(CatBoostRegressor(
+        task_type='GPU',
+        verbose=10
+    ), params)
+    model.fit(train_x, train_y)
+    print(f"Best estimator: {model.best_params_})")
+    print(f"MultiRMSE: {model.best_score_}")
+    print("Test score:", model.score(test_x, test_y))
 
 
 if __name__ == '__main__':
