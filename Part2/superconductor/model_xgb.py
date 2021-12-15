49ef76b0dd264ddbcb095fedfe5c5f8b101108e3
Mathew Simon <mat-simon@users.noreply.github.com>
2021-12-14 16:56:55 -0800

    need to update xgb regression, added PCA for emotions

--- Part2/superconductor/model_xgb.py
+++ Part2/superconductor/model_xgb.py
@@ -1,52 +1,48 @@
+import scipy.io
 import numpy as np
 import xgboost as xgb
-from numpy import genfromtxt
-from sklearn.metrics import accuracy_score, r2_score, mean_squared_error
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error as mse
 from sklearn.model_selection import train_test_split
+from numpy import genfromtxt
+import matplotlib.pyplot as plt
 
 
 def main():
-    print("Loading data...")
     csv_np = genfromtxt('data/train.csv', delimiter=',')
     full_x = csv_np[1:, :-1]
     full_y = csv_np[1:, -1]
     print(f"full_x: {full_x.shape}")
+    print(min(full_y))
+    print(max(full_y))
+    print(np.average(full_y))
 
     # split data into train and test
-    train_x, test_x, train_y, test_y = train_test_split(full_x,
+    train_x, val_x, train_y, val_y = train_test_split(full_x,
                                                         full_y,
                                                         test_size=0.25,
                                                         random_state=42)
-    print(f"test_x: {test_x.shape}")
-    # split train further into train and validate
-    train_80_x, val_x, train_80_y, val_y = train_test_split(train_x,
-                                                            train_y,
-                                                            test_size=0.25,
-                                                            random_state=0)
+
+    train_x, test_x, train_y, test_y = train_test_split(val_x,
+                                                      val_y,
+                                                      test_size=0.25,
+                                                      random_state=42)
+    print(f"test_x: {val_x.shape}")
+    # # split train further into train and validate
+    # train_x, val_x, train_y, val_y = train_test_split(train_x,
+    #                                                   train_y,
+    #                                                   test_size=0.25,
+    #                                                   random_state=0)
+    # print(f"train_x: {train_x.shape}")
+    # print(f"val_x: {val_x.shape}")
+
     train = xgb.DMatrix(train_x, train_y)
-    train_80x = xgb.DMatrix(train_80_x, train_80_y)
-    test = xgb.DMatrix(test_x, test_y)
-    print(f"train_x: {train_x.shape}")
-    print(f"val_x: {val_x.shape}")
 
-    #
-    # # Default params
-    # # for i in range(0, 1000):
-    # train_acc = []
-    # val_acc = []
-
-    params = {
-        'objective': 'multi:softmax',
-        'max_depth': 5,
-        'eval_metric': 'rmse',
-        'eta': .3,
-        'gpu_id': 0,
-        'tree_method': 'gpu_hist'
-    }
-
-    model = xgb.train(params, train, 350)
-    print("train error", 1-accuracy_score(train_y, model.predict(train)))
-    print("test error:", 1-accuracy_score(val_y, model.predict(val)))
+    regressor_results = xgb.XGBRegressor(max_depth=5, n_estimators=700, n_jobs=2,
+    objective='reg:squarederror', booster='gbtree',
+    random_state=42, learning_rate=0.19)
 
     # params = {'num_class': 10, 'max_depth': 5, 'eta': .3, 'objective': 'multi:softmax', 'gpu_id': 0,
     #           'tree_method': 'gpu_hist'}
@@ -61,29 +57,44 @@ def main():
 
     # min_merror = float("Inf")
     # best_params = None
-    # for eta in [.3]:
-    #     print("CV with eta={}".format(eta))
-    #     # update params
-    #     params['eta'] = eta
-    #     # Run CV
-    #     cv_results = xgb.cv(
-    #         params,
-    #         train,
-    #         num_boost_round=num_round,
-    #         seed=8,
-    #         nfold=5,
-    #         metrics=['merror'],
-    #         early_stopping_rounds=50,
-    #         verbose_eval=10
-    #     )
-    #     # Update best score
-    #     mean_merror = min(cv_results['test-merror-mean'])
-    #     boost_rounds = cv_results['test-merror-mean'].argmin()
-    #     print("\tmerror {} for {} rounds\n".format(mean_merror, boost_rounds))
-    #     if mean_merror < min_merror:
-    #         min_merror = mean_merror
-    #         best_params = eta
-    #         print("Best params: {}, merror: {}".format(best_params, min_merror))
+    # # for eta in [.3]:
+    # #     print("CV with eta={}".format(eta))
+    # #     # update params
+    # #     params['eta'] = eta
+    # #     # Run CV
+    # #     regressor_results = xgb.XGBRegressor(
+    # #         params,
+    # #         train,
+    # #         num_boost_round=num_round,
+    # #         seed=8,
+    # #         nfold=5,
+    # #         early_stopping_rounds=50,
+    # #         verbose_eval=10
+    # #     )
+        # Update best score
+
+    regressor_results.fit(train_x, train_y)
+
+    y_pred = regressor_results.predict(val_x)  # Predictions
+    y_true = val_y  # True values
+
+    MSE = mse(y_true, y_pred)
+    RMSE = np.sqrt(MSE)
+
+    R_squared = r2_score(y_true, y_pred)
+
+    print("\nRMSE: ", RMSE)
+    print()
+    print("R-Squared: ", R_squared)
+
+    print(regressor_results.score(test_x, test_y))
+        # mean_RMSE = min(regressor_results['test-RMSE-mean'])
+        # boost_rounds = regressor_results['test-RMSE-mean'].argmin()
+        # print("\tRMSE {} for {} rounds\n".format(mean_RMSE, boost_rounds))
+        # if mean_RMSE < min_RMSE:
+        #     min_RMSE = mean_RMSE
+        #     best_params = eta
+        #     print("Best params: {}, RMSE: {}".format(best_params, min_RMSE))
 
 
 if __name__ == '__main__':
